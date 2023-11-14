27e06772cd41d93fa33f72ea0889bcdde3a00f87
bujosa <davidbujosa@gmail.com>
2023-11-14 00:39:35 -0400

    feat: create initial hashing method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use sha2::{Digest, Sha256};
+
 fn main() {
-    println!("Hello, world!");
+    let url = "https://github.com/bujosa";
+    let mut hasher = Sha256::new();
+    hasher.update(url.as_bytes());
+    let result = hasher.finalize();
+    let short_id = hex::encode(result)[0..7].to_string();
+    println!("Generated short ID: {}", short_id);
 }
