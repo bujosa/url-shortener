c1e7ea9f8b950f86ce80dce219fce9792b6d6b67
bujosa <davidbujosa@gmail.com>
2023-11-14 20:01:32 -0400

    feat: add app state

---
+++ src/common/types/app_state.rs
@@ -0,0 +1,9 @@
+use std::sync::Arc;
+
+use crate::common::redis::redis::RedisClient;
+
+/// Represents the state of the application, including a Redis client.
+pub struct AppState {
+    /// An Arc-wrapped Redis client.
+    pub redis_client: Arc<RedisClient>,
+}
